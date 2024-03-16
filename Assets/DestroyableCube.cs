320d3ba738cc55aef38b59f10a7cf1bcee55813d
Nitsan Caduri <nitsan447@gmail.com>
2024-03-16 19:46:23 +0200

    Working on destructable cube

--- Assets/DestroyableCube.cs
+++ Assets/DestroyableCube.cs
@@ -2,17 +2,26 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class DestroyableCube : MonoBehaviour
 {
     [SerializeField] private int hitsToDestroy;
+    [SerializeField] private Sprite[] _sprites;
+    private int _currentSpriteIndex = 0;
+    [SerializeField] private GameObject _destructionParticlesPrefab;
 
     public void TakeDamage()
     {
         hitsToDestroy--;
         if (hitsToDestroy == 0)
         {
+            Instantiate(_destructionParticlesPrefab, transform.position, Quaternion.identity);
             Destroy(gameObject);
+            return;
         }
+
+        _currentSpriteIndex++;
+        GetComponent<SpriteRenderer>().sprite = _sprites[_currentSpriteIndex];
     }
 }
