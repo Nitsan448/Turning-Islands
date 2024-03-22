ce7aff356ab6915fb51f07f2d243df0cb2e92752
Nitsan Caduri <nitsan447@gmail.com>
2024-03-22 22:45:53 +0200

    Switched tutorials to timelines

--- Assets/FinishTutorialButton.cs
+++ Assets/FinishTutorialButton.cs
@@ -1,19 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class FinishTutorialButton : MonoBehaviour
 {
-    private static bool TutorialShown = false;
-
-    // Start is called before the first frame update
     void Start()
     {
-        if (TutorialShown)
-        {
-            Destroy(transform.parent.parent.gameObject);
-        }
+        // if ()
+        // {
+        //     Destroy(transform.parent.parent.gameObject);
+        // }
 
         Managers.Game.SetStateToTutorial();
         GetComponent<Button>().onClick.AddListener(OnClick);
