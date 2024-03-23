40e859abcf68b0c3e6faaecda83b8a8a672cbec5
Nitsan Caduri <nitsan447@gmail.com>
2024-03-23 21:53:39 +0200

    Working on new levels

--- Assets/_Scripts/Environment/Tube.cs
+++ Assets/_Scripts/Environment/Tube.cs
@@ -4,29 +4,30 @@ using UnityEngine;
 
 public class Tube : CubeFace
 {
-	public Tube ConnectedTube;
-	public bool Turned;
-	public float TimeInsideTube = 0.3f;
-	private float _tubeDisableTime = 0.7f;
-	protected override string SoundName { get; set; } = "No sound";
+    public Tube ConnectedTube;
+    public bool Turned;
+    public float TimeInsideTube = 0.3f;
+    private float _tubeDisableTime = 0.7f;
+    protected override string SoundName { get; set; } = "No sound";
 
-	protected override void OnCollisionOrTrigger(Ball ball)
-	{
-		StartCoroutine(SwitchTube(ball));
-	}
+    protected override void OnCollisionOrTrigger(Ball ball)
+    {
+        StartCoroutine(SwitchTube(ball));
+    }
 
-	private IEnumerator SwitchTube(Ball ball)
-	{
-		ball.gameObject.GetComponent<SpriteRenderer>().enabled = false;
-		yield return new WaitForSeconds(TimeInsideTube);
+    private IEnumerator SwitchTube(Ball ball)
+    {
+        SpriteRenderer ballSpriteRenderer = ball.gameObject.GetComponentInChildren<SpriteRenderer>();
+        ballSpriteRenderer.enabled = false;
+        yield return new WaitForSeconds(TimeInsideTube);
 
-		ConnectedTube.GetComponent<BoxCollider2D>().enabled = false;
-		Vector3 newPosition = ConnectedTube.transform.position;
-		ball.transform.position = new Vector3(newPosition.x, newPosition.y + 0.1f, newPosition.z);
-		ball.ChangeVelocity(ConnectedTube.GetComponent<CubeFace>().GetVelocity());
-		ball.gameObject.GetComponent<SpriteRenderer>().enabled = true;
-		yield return new WaitForSeconds(_tubeDisableTime);
+        ConnectedTube.GetComponent<BoxCollider2D>().enabled = false;
+        Vector3 newPosition = ConnectedTube.transform.position;
+        ball.transform.position = new Vector3(newPosition.x, newPosition.y + 0.1f, newPosition.z);
+        ball.ChangeVelocity(ConnectedTube.GetComponent<CubeFace>().GetVelocity());
+        ballSpriteRenderer.enabled = true;
+        yield return new WaitForSeconds(_tubeDisableTime);
 
-		ConnectedTube.GetComponent<BoxCollider2D>().enabled = true;
-	}
+        ConnectedTube.GetComponent<BoxCollider2D>().enabled = true;
+    }
 }
