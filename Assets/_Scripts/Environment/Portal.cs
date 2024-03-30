595864e13f4c9539a02d520038ea5359c4781bd0
Nitsan Caduri <nitsan447@gmail.com>
2024-03-30 14:51:21 +0300

    Worked on enter portal effect

--- Assets/_Scripts/Environment/Portal.cs
+++ Assets/_Scripts/Environment/Portal.cs
@@ -64,12 +64,29 @@ public class Portal : CubeFace
         Vector3 newPosition = ConnectedPortal.transform.position;
         ball.transform.position = new Vector3(newPosition.x, newPosition.y, newPosition.z);
         ball.ChangeVelocity(ConnectedPortal.GetVelocity());
+        UpdatePortalColors(5.5f);
 
-        yield return new WaitForSeconds(portalDisableTime);
+        float currentTime = 0;
+        while (currentTime < portalDisableTime)
+        {
+            if (currentTime / portalDisableTime < 0.3f)
+            {
+                UpdatePortalColors(Mathf.Lerp(4, 7, currentTime / portalDisableTime));
+            }
+            else
+            {
+                UpdatePortalColors(Mathf.Lerp(7, 4, currentTime / portalDisableTime));
+            }
+
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+
+        UpdatePortalColors();
         ConnectedPortal.Collider.enabled = true;
     }
 
-    public void UpdatePortalColors()
+    public void UpdatePortalColors(float glowIntensity = 4)
     {
         if (PortalIndex == -1)
         {
@@ -79,7 +96,7 @@ public class Portal : CubeFace
         MaterialPropertyBlock materialPropertyBlock = new();
         materialPropertyBlock.SetInt("_IsOpen", IsOpen ? 1 : 0);
         materialPropertyBlock.SetColor("_BaseColor", PortalColors.ColorByIndex[PortalIndex]);
-        materialPropertyBlock.SetColor("_GlowColor", PortalColors.ColorByIndex[PortalIndex] * 4);
+        materialPropertyBlock.SetColor("_GlowColor", PortalColors.ColorByIndex[PortalIndex] * glowIntensity);
 
         PortalGraphics.Light.color = PortalColors.ColorByIndex[PortalIndex];
         PortalGraphics.Sprite.SetPropertyBlock(materialPropertyBlock);
