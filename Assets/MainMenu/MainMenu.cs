e300d55db3b5e5592f60931ffa44cf0a9617dfc7
Nitsan Caduri <nitsan447@gmail.com>
2024-04-11 14:06:34 +0300

    Level select panel

--- Assets/MainMenu/MainMenu.cs
+++ Assets/MainMenu/MainMenu.cs
@@ -9,22 +9,15 @@ public class MainMenu : MonoBehaviour
 {
     [SerializeField] private GameObject _mainPanel;
 
-    [SerializeField] private GameObject _controlsPanel;
-
     [SerializeField] private GameObject _levelSelectPanel;
 
     [SerializeField] private Button _playButton;
-
-    [SerializeField] private Button _controlsButton;
 
     [SerializeField] private Button _levelSelectButton;
 
     [SerializeField] private Button _exitButton;
 
-    [SerializeField] private Button _controlsBackToMainPanelButton;
     [SerializeField] private Button _levelSelectBackToMainPanelButton;
-
-    [SerializeField] private TextMeshProUGUI _panelTitle;
 
     private void Awake()
     {
@@ -36,39 +29,18 @@ public class MainMenu : MonoBehaviour
             delegate { SceneManager.LoadScene(PlayerPrefs.GetInt("CurrentLevel", 1)); }
         );
 
-        _controlsButton.onClick.AddListener(
-            delegate
-            {
-                _controlsPanel.SetActive(true);
-                _panelTitle.text = "Controls";
-                _mainPanel.SetActive(false);
-            }
-        );
-
         _levelSelectButton.onClick.AddListener(
             delegate
             {
                 _levelSelectPanel.SetActive(true);
-                _panelTitle.text = "Level Select";
                 _mainPanel.SetActive(false);
             }
         );
 
-        _controlsBackToMainPanelButton.onClick.AddListener(
-            delegate
-            {
-                _mainPanel.SetActive(true);
-                _panelTitle.text = "Main Menu";
-                _controlsPanel.SetActive(false);
-                _levelSelectPanel.SetActive(false);
-            }
-        );
         _levelSelectBackToMainPanelButton.onClick.AddListener(
             delegate
             {
                 _mainPanel.SetActive(true);
-                _controlsPanel.SetActive(false);
-                _panelTitle.text = "Main Menu";
                 _levelSelectPanel.SetActive(false);
             }
         );
