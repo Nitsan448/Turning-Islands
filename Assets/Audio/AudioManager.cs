96827c8cd55328d2b023a1dc7a8b6d72c345c54d
Nitsan Caduri <nitsan447@gmail.com>
2024-04-20 18:27:05 +0300

    Added basic debug console commands

--- Assets/Audio/AudioManager.cs
+++ Assets/Audio/AudioManager.cs
@@ -7,27 +7,33 @@ using System;
 public class AudioManager : MonoBehaviour, IGameManager
 {
     [SerializeField] private Sound[] _sounds;
+    [SerializeField] private bool _playAudio1 = true;
 
     public void Startup()
     {
-        foreach(Sound sound in _sounds)
-		{
+        foreach (Sound sound in _sounds)
+        {
             sound.AudioSource = gameObject.AddComponent<AudioSource>();
             sound.AudioSource.clip = sound.AudioClip;
             sound.AudioSource.volume = sound.Volume;
-		}
+        }
     }
 
     public void PlaySound(string soundName)
-	{
+    {
+        if (!_playAudio1)
+        {
+            return;
+        }
+
         Sound sound = Array.Find(_sounds, sound => sound.Name == soundName);
-        if(sound != null)
-		{
+        if (sound != null)
+        {
             sound.AudioSource.Play();
-		}
-		else
-		{
+        }
+        else
+        {
             Debug.LogWarning("Could not find sound: " + soundName);
-		}
-	}
+        }
+    }
 }
---
+++ Assets/DebugManager.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+public class DebugManager : MonoBehaviour
+{
+    private Dictionary<string, Action> _commands;
+    private string _currentInput = "";
+
+    private void Start()
+    {
+        _commands = new() { { "next", Managers.Game.GoToNextLevel } };
+        for (int levelIndex = 0; levelIndex < SceneManager.sceneCountInBuildSettings; levelIndex++)
+        {
+            //TODO: go over all scenes and add ones who's name starts with level
+            int levelNumber = levelIndex + 1;
+            _commands.Add("level" + (levelNumber), delegate { SceneManager.LoadScene("Level" + (levelNumber)); });
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SendInput();
+            _currentInput = "";
+        }
+
+        _currentInput += Input.inputString;
+    }
+
+    private void SendInput()
+    {
+        if (_commands.Keys.Contains(_currentInput))
+        {
+            _commands[_currentInput]();
+        }
+    }
+}
+#endif
